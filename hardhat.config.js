baddfa0ab860ae740efbf64c1f692eb798faa79e
Anoushk Kharangate <anoushka1828@gmail.com>
2021-10-30 12:56:35 +0530

    hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,20 +2,17 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby-light.eth.linkpool.io/',
+      accounts: ['844dac72a2a5d7206f35ee6b3a8c92c494ee2311569e7fe99f5a6c31debd783c'],
+    },
+  },
 };
